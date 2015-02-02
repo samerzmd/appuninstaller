f86d878a7aeeaedf9723ce17743d330aed877c94
Mahdi <mahdi@Mahdis-Mac-mini.local>
2015-02-02 12:44:16 +0200

    refactor the app name

--- app/src/androidTest/java/com/appuninstaller/ApplicationTest.java
+++ app/src/androidTest/java/com/appuninstaller/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.batchuninstaller;
+package com.appuninstaller;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/appuninstaller/ui/MainActivity.java
+++ app/src/main/java/com/appuninstaller/ui/MainActivity.java
@@ -1,11 +1,12 @@
-package com.batchuninstaller.ui;
+package com.appuninstaller.ui;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.batchuninstaller.R;
+import com.appuninstaller.R;
+
 
 public class MainActivity extends ActionBarActivity {
 
